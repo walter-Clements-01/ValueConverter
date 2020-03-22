1d759fca61d8ad793dc0439f85b93c9ceb69325c
Francesco <francesco.boccuni728@gmail.com>
2020-03-22 19:31:02 +0100

    added JComboBox listener added 2nd JTextField conversion modified graphics

--- src/main/java/valueC/MyPanel.java
+++ src/main/java/valueC/MyPanel.java
@@ -1,13 +1,13 @@
 package valueC;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import javax.swing.*;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Map;
 
 public class MyPanel extends JPanel
@@ -17,8 +17,7 @@ public class MyPanel extends JPanel
     private JComboBox boxInit;
     private JComboBox boxFin;
     private JTextField startTextField;
-    private JLabel endLabel;
-    private JButton convButton;
+    private JTextField endTextField;
     private JPanel topPanel;
     private JPanel middlePanel;
     private JPanel bottomPanel;
@@ -32,81 +31,47 @@ public class MyPanel extends JPanel
         rates=currencies.getRates();
         names=currencies.getRatesNames();
 
-        topPanel = new JPanel(new BorderLayout());
+        topPanel = new JPanel(new GridLayout(1,2,20,20));
         viLabel = new JLabel("Valuta da convertire");
-        viLabel.setFont(new Font(null,Font.BOLD, 20));
+        viLabel.setFont(new Font(null,Font.BOLD, 30));
         vfLabel = new JLabel("Valuta convertita");
-        vfLabel.setFont(new Font(null,Font.BOLD, 20));
-        topPanel.add(viLabel, BorderLayout.WEST);
-        topPanel.add(vfLabel, BorderLayout.EAST);
+        vfLabel.setFont(new Font(null,Font.BOLD, 30));
+        topPanel.add(viLabel, BorderLayout.CENTER);
+        topPanel.add(vfLabel, BorderLayout.CENTER);
 
-        middlePanel = new JPanel(new GridLayout(1,3,20,20));
+        middlePanel = new JPanel(new GridLayout(1,2,20,20));
         boxInit = new JComboBox();
-        boxInit.setSize(100,20);
+        boxInit.setSize(100,75);
         boxInit.setFont(new Font(null,Font.BOLD, 15));
-        convButton = new JButton("Converti");
-        convButton.setFont(new Font(null,Font.BOLD, 20));
         boxFin = new JComboBox();
         boxFin.setFont(new Font(null,Font.BOLD, 15));
-        boxFin.setSize(100,20);
+        boxFin.setSize(100,75);
         middlePanel.add(boxInit);
-        middlePanel.add(convButton);
         middlePanel.add(boxFin);
         setComboBoxes();
 
-        //convButton.addActionListener(new MyListener());
+        bottomPanel = new JPanel(new GridLayout(1,2,20,20));
 
-        bottomPanel = new JPanel(new BorderLayout());
         startTextField = new JTextField();
-        startTextField.setPreferredSize(new Dimension(200,20));
-        endLabel = new JLabel();
-        endLabel.setPreferredSize(new Dimension(200,20));
-        endLabel.setFont(new Font(null,Font.BOLD, 20));
+        startTextField.setPreferredSize(new Dimension(200,75));
         startTextField.setFont(new Font(null,Font.BOLD, 20));
+
+        endTextField = new JTextField();
+        endTextField.setPreferredSize(new Dimension(200,75));
+        endTextField.setFont(new Font(null,Font.BOLD, 20));
+
         bottomPanel.add(startTextField, BorderLayout.WEST);
-        bottomPanel.add(endLabel, BorderLayout.EAST);
+        bottomPanel.add(endTextField, BorderLayout.EAST);
 
         setLayout(new GridLayout(3,1,20,20));
         add(topPanel);
         add(middlePanel);
         add(bottomPanel);
 
-        startTextField.getDocument().addDocumentListener(new DocumentListener() {
-
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                exeConversion();
-            }
-
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                exeConversion();
-            }
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                exeConversion();
-            }
-            private void exeConversion() {
-                Double toConvert;
-                try{
-                    toConvert = Double.valueOf(startTextField.getText());
-                    Double converted=getConversion(toConvert,names.get(boxInit.getSelectedItem().toString()),names.get(boxFin.getSelectedItem().toString()));
-                    setEndLabel(converted.toString());
-                }
-                catch(NumberFormatException e){
-                    setEndLabel("0");
-                }
-            }
-            private Double getConversion(Double toConvert, Double startCoef, Double endCoef)
-            {
-                return (1/startCoef*toConvert)*endCoef;
-            }
-            private void setEndLabel(String converted)
-            {
-                endLabel.setText(converted);
-            }
-        });
+        boxInit.addItemListener(new ItemChangeListener());
+        boxFin.addItemListener(new ItemChangeListener());
+        startTextField.getDocument().addDocumentListener(new TextChangeListener("start"));
+        endTextField.getDocument().addDocumentListener(new TextChangeListener("end"));
     }
    public void setComboBoxes() throws Exception {
        for (Map.Entry<String, Double> entry : names.entrySet()) {
@@ -123,5 +88,77 @@ public class MyPanel extends JPanel
         comboBoxes.setBoxInit(boxInit.getSelectedItem().toString());
         comboBoxes.setBoxFin(boxFin.getSelectedItem().toString());
         return comboBoxes;
+    }
+
+    public void exeConversion(String textField) {
+        Double toConvert;
+        try{
+            if(textField.equals("start"))
+            {
+                toConvert = Double.valueOf(startTextField.getText());
+                Double converted=getConversion(toConvert,names.get(boxInit.getSelectedItem().toString()),names.get(boxFin.getSelectedItem().toString()));
+                setTextField(converted.toString(),textField);
+            }
+            else
+            {
+                toConvert = Double.valueOf(endTextField.getText());
+                Double converted=getConversion(toConvert,names.get(boxFin.getSelectedItem().toString()),names.get(boxInit.getSelectedItem().toString()));
+                setTextField(converted.toString(),textField);
+            }
+
+        }
+        catch(NumberFormatException e){
+            setTextField("0",textField);
+        }
+    }
+    public Double getConversion(Double toConvert, Double startCoef, Double endCoef)
+    {
+        return (1/startCoef*toConvert)*endCoef;
+    }
+    public void setTextField(String converted, String textField)
+    {
+        if(textField.equals("start"))
+        {
+            try
+            {
+                endTextField.setText(converted);
+            }
+            catch(IllegalStateException e)
+            {
+                System.out.println("Caught Start");
+            }
+        }
+        else
+        {
+            try
+            {
+                startTextField.setText(converted);
+            }
+            catch(IllegalStateException e)
+            {
+                System.out.println("Caught End");
+            }
+        }
+    }
+    private class TextChangeListener implements DocumentListener
+    {
+        String textField;
+        public TextChangeListener(String textField)
+        {
+            this.textField=textField;
+        }
+        @Override
+        public void insertUpdate(DocumentEvent e) { exeConversion(textField); }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) { exeConversion(textField); }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) { exeConversion(textField); }
+    }
+    private class ItemChangeListener implements ItemListener
+    {
+        @Override
+        public void itemStateChanged(ItemEvent e) { exeConversion("start"); }
     }
 }
